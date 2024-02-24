0f3f9b802c4ad675c040ce1c038745b5f159848c
bilal-sagir <sagir.bilal@icloud.com>
2024-02-24 23:08:16 +0300

    Initial Commit

---
+++ PinchSwiftUI/PinchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchSwiftUIApp.swift
+//  PinchSwiftUI
+//
+//  Created by Bilal on 24.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct PinchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
