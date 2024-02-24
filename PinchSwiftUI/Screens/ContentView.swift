ebef30e53125523b2451d30e0988399afe7ec04f
bilal-sagir <sagir.bilal@icloud.com>
2024-02-25 00:26:25 +0300

    added double tab gesture to image

--- PinchSwiftUI/Screens/ContentView.swift
+++ PinchSwiftUI/Screens/ContentView.swift
@@ -8,17 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - PROPERTIES
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    // MARK: - FUNCTIONS
+    // MARK: - CONTENT
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack {
+            ZStack {
+                // MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(
+                        .rect(cornerRadii: RectangleCornerRadii(topLeading: 10,
+                                                                       bottomLeading: 10,
+                                                                       bottomTrailing: 10,
+                                                                       topTrailing: 10))
+                    )
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .animation(.linear(duration: 1), value: isAnimating)
+                    .scaleEffect(imageScale)
+                    // MARK: - 1. TAP GESTURE
+                    .onTapGesture(count: 2, perform: {
+                        withAnimation(.spring()) {
+                            imageScale = imageScale == 1 ? 5 : 1
+                        }
+                    })
+            } //: ZSTACK
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                isAnimating = true
+            })
+        } //: NAVIGATION
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
